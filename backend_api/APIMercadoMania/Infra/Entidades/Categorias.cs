ff43181ecf18b8c738b10619aa8a6e803a728c4c
Vinícius Souza <vinigsouza.bnu@gmail.com>
2021-09-30 22:49:57 -0300

    Criação da Migration do Banco de Dados

--- backend_api/APIMercadoMania/Infra/Entidades/Categorias.cs
+++ backend_api/APIMercadoMania/Infra/Entidades/Categorias.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Infra.Entidades
 {
-    class Categoria
+    public class Categorias
     {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
     }
 }
