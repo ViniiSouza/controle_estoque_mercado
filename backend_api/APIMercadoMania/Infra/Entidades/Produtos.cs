33daecb71a73cb1e0d357e4a041c0913111b0d47
Vinícius Souza <vinigsouza.bnu@gmail.com>
2021-09-30 22:41:34 -0300

    Criação do Infra e Configuração do CORS na API

---
+++ backend_api/APIMercadoMania/Infra/Entidades/Produtos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Infra.Entidades
+{
+    public class Produtos
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public int CategoriaId { get; set; }
+        public Categorias Categoria { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
---
+++ backend_api/APIMercadoMania/Infra/MercadoManiaDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Infra.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infra
+{
+    
+    public class MercadoManiaDbContext : DbContext
+    {
+        public string connectionString = @"Server=DESKTOP-OF89FLJ\SQLEXPRESS;Database=MercadoMania;Trusted_Connection=True;";
+        public MercadoManiaDbContext() : base() {}
+        public virtual DbSet<Categorias> Categorias { get; set; }
+        public virtual DbSet<Produtos> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+    }
+}
