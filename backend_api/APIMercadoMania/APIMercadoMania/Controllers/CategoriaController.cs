102ee22207eaaf281ce14ac5b689225a14fdd2b9
Vinícius Souza <vinigsouza.bnu@gmail.com>
2021-10-02 17:30:54 -0300

    Criação das Controllers e conclusão da API

---
+++ backend_api/APIMercadoMania/APIMercadoMania/Controllers/CategoriaController.cs
@@ -0,0 +1,71 @@
+﻿using Aplicacao;
+using Infra.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace APIMercadoMania.Controllers
+{
+    [ApiController]
+    [Route("{controller}")]
+    public class CategoriaController : ControllerBase
+    {
+        private CategoriaService _categoriaService = new CategoriaService();
+
+        [HttpGet]
+        public List<Categorias> Get()
+        {
+            return _categoriaService.Get().ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Categorias GetCategoria(int id)
+        {
+            return _categoriaService.GetById(id);
+        }
+
+        [HttpPost]
+        public string Post([FromBody]Categorias categoria)
+        {
+            try
+            {
+                _categoriaService.Post(categoria);
+                return "Post executado";
+            }
+            catch (Exception ex)
+            {
+                return "Erro encontrado: " + ex.Message;
+            }
+            
+        }
+        [HttpPut]
+        public string Put(Categorias categoria)
+        {
+            try
+            {
+                _categoriaService.Put(categoria);
+                return "Put executado";
+            }
+            catch (Exception ex)
+            {
+                return "Erro encontrado: " + ex.Message;
+            }
+            
+        }
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            try
+            {
+                _categoriaService.Delete(_categoriaService.GetById(id));
+                return "Delete executado";
+            }
+            catch (Exception ex)
+            {
+                return "Erro encontrado: " + ex.Message;
+            }
+            
+        }
+    }
+}
---
+++ backend_api/APIMercadoMania/APIMercadoMania/Controllers/ProdutoController.cs
@@ -0,0 +1,67 @@
+﻿using Aplicacao;
+using Infra.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIMercadoMania.Controllers
+{
+    [ApiController]
+    [Route("{controller}")]
+    public class ProdutoController : ControllerBase
+    {
+        private ProdutoService _produtoService = new ProdutoService();
+        [HttpGet]
+        public List<Produtos> Get()
+        {
+            return _produtoService.Get().ToList();
+        }
+        [HttpGet("{id}")]
+        public Produtos GetProduto(int id)
+        {
+            return _produtoService.GetById(id);
+        }
+        [HttpPost]
+        public string Post([FromBody]Produtos produto)
+        {
+            try
+            {
+                _produtoService.Post(produto);
+                return "Post executado";
+            }
+            catch (Exception ex)
+            {
+                return "Erro encontrado: " + ex.Message;
+            }
+        }
+        [HttpPut]
+        public string Put(Produtos produto)
+        {
+            try
+            {
+                _produtoService.Put(produto);
+                return "Put executado";
+            }
+            catch (Exception ex)
+            {
+                return "Erro encontrado: " + ex.Message;
+            }
+        }
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            try
+            {
+                _produtoService.Delete(_produtoService.GetById(id));
+                return "Delete executado";
+            }
+            catch (Exception ex)
+            {
+                return "Erro encontrado: " + ex.Message;
+            }
+            
+        }
+    }
+}
